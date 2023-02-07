735f1c061fe650e3ff78bea8f1bf903f759c05e7
opixelum <48217573+opixelum@users.noreply.github.com>
2023-02-08 00:12:23 +0100

    Import onActivate() from gui.h

--- src/main.c
+++ src/main.c
@@ -1,19 +1,4 @@
-// Include gtk
-#include <gtk/gtk.h>
-
-static void onActivate(GtkApplication *app)
-{
-    // Create a new window
-    GtkWidget *window = gtk_application_window_new(app);
-
-    // Create a new button
-    GtkWidget *button = gtk_button_new_with_label("Hello, World!");
-
-    // When the button is clicked, close the window passed as an argument
-    g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_window_close), window);
-    gtk_window_set_child(GTK_WINDOW(window), button);
-    gtk_window_present(GTK_WINDOW(window));
-}
+#include "gui.h"
 
 int main(int argc, char *argv[])
 {
