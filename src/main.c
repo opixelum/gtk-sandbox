adbd963ba0376bfa2ae8fbd85662674537aa20c3
opixelum <48217573+opixelum@users.noreply.github.com>
2023-02-08 01:14:55 +0100

    Improve variable naming

--- src/main.c
+++ src/main.c
@@ -3,12 +3,12 @@
 int main(int argc, char *argv[])
 {
     // Create a new application
-    GtkApplication *app = gtk_application_new
+    GtkApplication *application = gtk_application_new
     (
         "com.example.GtkApplication",
         G_APPLICATION_FLAGS_NONE
     );
-    g_signal_connect(app, "activate", G_CALLBACK(onActivate), NULL);
+    g_signal_connect(application, "activate", G_CALLBACK(onActivate), NULL);
 
-    return g_application_run(G_APPLICATION(app), argc, argv);
+    return g_application_run(G_APPLICATION(application), argc, argv);
 }
